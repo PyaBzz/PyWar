f0f6b047a0ab8cd85f1231dbee841f4295b8f404
PyaBzz <>
2019-12-12 20:02:09 +0000

    Replace lnk files with bat files

---
+++ 01-Run-PyGunGame.bat
@@ -0,0 +1,4 @@
+echo off
+cls
+
+start iw3mp.exe +set fs_game Mods/PyGunGame +exec PyGunGame +map_rotate
---
+++ 03-Run-PyWar-Snipe.bat
@@ -0,0 +1,4 @@
+echo off
+cls
+
+start iw3mp.exe +set fs_game Mods/PyWar +exec Base +exec Sniper +map_rotate
