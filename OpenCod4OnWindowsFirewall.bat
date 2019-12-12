d1d50e61037714c78f58600c6458aae393b563e3
Pouya <pooya.bazzaz@geeks.ltd.uk>
2019-12-12 17:50:55 +0000

    Change port numbers

--- OpenCod4OnWindowsFirewall.bat
+++ OpenCod4OnWindowsFirewall.bat
@@ -1,12 +1,12 @@
 echo off
 cls
 
-echo Opening TCP 28960,8283
+echo Opening TCP 8283 for fast download from IIS
 
-netsh advfirewall firewall add rule name="PyCod4Tcp" dir=in action=allow protocol=TCP localport=28960,8283
+netsh advfirewall firewall add rule name="PyWarCod4Tcp" dir=in action=allow protocol=TCP localport=8283
 
-echo Opening UDP 28960,8283
+echo Opening UDP 8283 for IIS and 28960,20800,20810 for the game
 
-netsh advfirewall firewall add rule name="PyCod4Udp" dir=in action=allow protocol=UDP localport=28960,8283
+netsh advfirewall firewall add rule name="PyWarCod4Udp" dir=in action=allow protocol=UDP localport=28960,20800,20810
 
 pause
