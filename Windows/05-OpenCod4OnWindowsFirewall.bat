ecc9787b66f89017b6b82f5e2881fca178aaefc6
Pouya <pooya.bazzaz@geeks.ltd.uk>
2020-01-27 13:28:35 +0000

    Small correction

--- Windows/05-OpenCod4OnWindowsFirewall.bat
+++ Windows/05-OpenCod4OnWindowsFirewall.bat
@@ -5,7 +5,7 @@ echo Opening TCP 8283 for fast download from IIS
 
 netsh advfirewall firewall add rule name="PyWarCod4Tcp" dir=in action=allow protocol=TCP localport=8283
 
-echo Opening UDP 8283 for IIS and 28960,20800,20810 for the game
+echo Opening UDP 28960,20800,20810 for the game
 
 netsh advfirewall firewall add rule name="PyWarCod4Udp" dir=in action=allow protocol=UDP localport=28960,20800,20810
 
